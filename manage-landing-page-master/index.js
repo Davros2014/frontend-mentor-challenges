293393f147809edb16e1fa2357101408aef21e76
David Parsons <info@dpdesigns.eu>
2020-09-30 21:25:25 +0200

    working on html structure, integrating scss

--- manage-landing-page-master/index.js
+++ manage-landing-page-master/index.js
@@ -1,1 +1,24 @@
-console.log("yo");
+///////////////////////////////////////////
+// TOGGLE MOBILE MENU
+///////////////////////////////////////////
+
+function toggleIt() {
+    document.getElementById("mobileNav").classList.toggle("active");
+}
+
+const mobileView = () => {
+    document.querySelector(".desktopNav").id = "mobileNav";
+};
+// add mobile id
+if (window.innerWidth <= 999) {
+    document.querySelector(".desktopNav").id = "mobileNav";
+}
+// add & remove mobile id based on screen width
+const mobileOrDesktop = () => {
+    if (window.innerWidth <= 999) {
+        mobileView();
+    } else if (window.innerWidth >= 1000) {
+        document.querySelector(".desktopNav").removeAttribute("id");
+    }
+};
+window.addEventListener("resize", mobileOrDesktop);
